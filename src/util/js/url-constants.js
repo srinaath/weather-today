27c5092a3963ede95b8c12134f76082f9cc8e38e
Srinaath Ravichandran <sravichandran@deloitte.com>
2018-06-23 18:08:43 -0600

    Updated url constants

--- src/util/js/url-constants.js
+++ src/util/js/url-constants.js
@@ -1,7 +1,8 @@
-import { setItem } from './session-storage';
-
 let constants = {
-  BASE_URL: '',
-  ADDRESS_SEARCH_URL: ''
+  BASE_URL: 'https://www.metaweather.com/api/',
+  GOOGLE_ADDRESS_SEARCH_URL: 'https://maps.googleapis.com/maps/api/geocode/json',
+  GOOGLE_API_KEY: 'AIzaSyDHgwYiOsSK5Ag2F_ID5kuAAj-bfwcEGqI',
+  'WELL_KNOW_REQUEST_URL': 'location/search',
+  'LOCATION_SEARCH_URL': 'location/'
 };
 export default constants;
